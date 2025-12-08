a32f7235fc50759afa3c5266597481234f5a835b
djangify <djangify@gmail.com>
2025-12-08 08:35:17 +0000

    Add homepage to adminita and change demo homepage. update change_form - slug now working

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 # Customize admin site
 admin.site.site_header = "Adminita Demo"
@@ -7,5 +8,6 @@ admin.site.site_title = "Adminita Admin Portal"
 admin.site.index_title = "Welcome to Adminita"
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("admin/", admin.site.urls),
 ]
