07f06fe613f1e50360f428065ac3ec98ea555451
djangify <djangify@gmail.com>
2025-12-25 14:29:47 +0000

    Fixing toggle and delete boxes on change_form.

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
 
 # Customize admin site
 admin.site.site_header = "Adminita Demo"
 admin.site.site_title = "Adminita Admin Portal"
 admin.site.index_title = "Welcome to Adminita"
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
